3ef1fd60d9303b611bacaff7243af5c726aa7b6b
Gustavo Morais <medeiros@discente.ufg.br>
2023-10-27 19:28:12 -0300

    atividade do dia 25/10

--- anotacao/src/Observable.java
+++ anotacao/src/Observable.java
@@ -1,32 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Observable {
     private String identificador;
-    private List<Class<?>> observers = new ArrayList<>();
+    private List<Consumer<Void>> observers = new ArrayList<>();
 
     public Observable(String identificador) {
         this.identificador = identificador;
     }
 
-    public void adicionarObserver(Object observer) {
-        Class<?> observerClass = observer.getClass();
-        if (observerClass.isAnnotationPresent(Observer.class)) {
-            Observer annotation = observerClass.getAnnotation(Observer.class);
-            if (annotation.observado().equals(identificador)) {
-                observers.add(observerClass);
-            } else {
-                System.out.println("Identificador do Observer não coincide com o identificador do Observable.");
-            }
+    public void adicionarObserver(String observado, Consumer<Void> observer) {
+        if (observado.equals(identificador)) {
+            observers.add(observer);
         } else {
-            System.out.println("Classe não é um Observer.");
+            System.out.println("Identificador do Observer não coincide com o identificador do Observable.");
         }
     }
 
     public void notificarObservers() {
-        for (Class<?> observerClass : observers) {
-            // Implemente aqui a lógica de notificação para cada observer.
-            // Você pode usar reflection para criar instâncias dos observers e chamar métodos apropriados.
+        for (Consumer<Void> observer : observers) {
+            observer.accept(null); // Notifica o observador
         }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Crie um objeto Observable com um identificador
+        Observable carroObservable = new Observable("carro");
+
+        // Adicione um observador usando uma expressão lambda
+        carroObservable.adicionarObserver("carro", observer -> {
+            System.out.println("Carro Observer foi notificado.");
+            // Adicione aqui a lógica específica para o observador do carro.
+        });
+
+        // Notifique os observadores
+        carroObservable.notificarObservers();
     }
 }
